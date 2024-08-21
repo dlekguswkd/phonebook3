5d89886fc89b02ea903e8776d60a3004986f84de
dlekguswkd <dlekguswkd@naver.com>
2024-08-21 12:59:07 +0900

    2024-08-21 서비스까지 추가

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.dao.PhonebookDao;
+import com.javaex.service.PhonebookService;
 import com.javaex.vo.PersonVo;
 
 
@@ -20,8 +21,11 @@ public class PhonebookController {	//제어권을 갖고있음 내가 줄게
 	//필드
 	//dao를 메모리에 올린다 (밑에 여러번 쓰이니까 필드로 만듬)
 	//private PhonebookDao phonebookDao = new PhonebookDao(); 원래는 이거지만 이건 내가 할게임 스프링의 장점 사용안하는거임
-	@Autowired		// 자동으로 연결해줘 -> dao에 @Repository 넣어주기 (스프링의 장점 사용하기)
-	private PhonebookDao phonebookDao;
+	//@Autowired		// 자동으로 연결해줘 -> dao에 @Repository 넣어주기 (스프링의 장점 사용하기)
+	//private PhonebookDao phonebookDao;
+	
+	@Autowired		// 자동으로 연결해줘 -> Service에 @Service 넣어주기
+	private PhonebookService phonebookService;
 	
 	//생성자
 	//메소드gs
@@ -40,8 +44,10 @@ public class PhonebookController {	//제어권을 갖고있음 내가 줄게
 		//dao를 메모리에 올린다 (위에 필드로 만들었음)
 		
 		//phonebookDao의 메소드를 이용해서 데이터를 가져온다 (vo에 PersonList 만들어놨음)
-		List<PersonVo> personList = phonebookDao.getPersonList();
-		System.out.println(personList);
+		//List<PersonVo> personList = phonebookDao.getPersonList();
+		//System.out.println(personList);
+		
+		List<PersonVo> personList = phonebookService.exeGetPerson();
 		
 		// 한다리 건너서 넣기 (데이터 보내기)
 		model.addAttribute("personList", personList);
@@ -77,12 +83,15 @@ public class PhonebookController {	//제어권을 갖고있음 내가 줄게
 		
 		
 		//phonebookDao의 메소드를 이용해서 저장한다
-		int count = phonebookDao.insertPerson(personVo);
-		System.out.println(count);
+		//int count = phonebookDao.insertPerson(personVo);
+		//System.out.println(count);
+		
+		phonebookService.exeWritePerson(personVo);
 		
 		//리스트로 리다이렉트(redirect:인터넷주소)
 		// "redirect:http://localhost:8888/phonebook3/list" 생략가능
 		return "redirect:/list";
+		
 		
 	}
 	
@@ -121,9 +130,10 @@ public class PhonebookController {	//제어권을 갖고있음 내가 줄게
 		
 		//dao를 메모리에 올린다 (위에 필드로 만들었음)
 		
-				
 		//phonebookDao의 메소드를 이용해서 데이터를 삭제한다
-		int count = phonebookDao.deletePerson(no);
+		//int count = phonebookDao.deletePerson(no);
+		
+		phonebookService.exeDeletePerson(no);
 		
 		//리스트로 리다이렉트(redirect:인터넷주소)
 		return "redirect:/list";
@@ -139,8 +149,10 @@ public class PhonebookController {	//제어권을 갖고있음 내가 줄게
 		System.out.println("PhonebookController.editForm()");
 		System.out.println(no);
 		
-		PersonVo personVo = phonebookDao.getPersonOne(no);
-		System.out.println(personVo);
+		//PersonVo personVo = phonebookDao.getPersonOne(no);
+		//System.out.println(personVo);
+		
+		PersonVo personVo = phonebookService.exeEditForm(no);
 		
 		model.addAttribute("personVo", personVo);
 		
@@ -163,8 +175,10 @@ public class PhonebookController {	//제어권을 갖고있음 내가 줄게
 		System.out.println(personVo);
 		
 		//phonebookDao의 메소드를 이용해서 저장한다
-		int count = phonebookDao.updatePerson(personVo);
-		System.out.println(count);
+		//int count = phonebookDao.updatePerson(personVo);
+		//System.out.println(count);
+		
+		phonebookService.exeEditPerson(personVo);
 		
 		//리스트로 리다이렉트(redirect:인터넷주소)
 		return "redirect:/list";
---
+++ src/main/java/com/javaex/service/PhonebookService.java
@@ -0,0 +1,74 @@
+package com.javaex.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaex.dao.PhonebookDao;
+import com.javaex.vo.PersonVo;
+
+@Service
+public class PhonebookService {
+	
+	@Autowired
+	private PhonebookDao phonebookDao;
+	
+	// 메소드 이름에 exe를 붙이면 서비스라는뜻 (그냥 우리가 알기쉽게)
+	
+	/* 전화번호 등록 */
+	public int exeWritePerson(PersonVo personVo) {	 
+		System.out.println("PhonebookService.exeWritePerson()");
+		
+		int count = phonebookDao.insertPerson(personVo);
+		
+		return count;
+		
+	}
+	
+	
+	/* 전화번호 전체 가져오기 */
+	public List<PersonVo> exeGetPerson() {
+		System.out.println("PhonebookService.exeGetPerson()");
+		
+		List<PersonVo> personList = phonebookDao.getPersonList();
+		
+		return personList;
+		
+	}
+	
+	
+	/* 전화번호 삭제 */
+	public int exeDeletePerson(int no) {
+		System.out.println("PhonebookService.exeDeletePerson()");
+		
+		int count = phonebookDao.deletePerson(no);
+		
+		return count;
+		
+	}
+	
+
+	/* 전화번호 수정폼 */
+	public PersonVo exeEditForm(int no) {
+		System.out.println("PhonebookService.exeEditPerson()");
+		
+		PersonVo personVo = phonebookDao.getPersonOne(no);
+	
+		return personVo;
+		
+	}
+	
+	
+	/* 전화번호 수정 */
+	public int exeEditPerson(PersonVo personVo) {
+		System.out.println("PhonebookService.exeEditPerson()");
+		
+		int count = phonebookDao.updatePerson(personVo);
+	
+		return count;
+		
+	}
+	
+	
+}
